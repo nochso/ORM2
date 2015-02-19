ee9c32629571d7a3698667af3e0df0d09292b8b5
Marcel Voigt <marcelpvoigt@gmail.com>
2015-02-20 00:18:49 +0100

    Extract function getPrettyStatement in DBA\LogEntry

--- src/DBA/LogEntry.php
+++ src/DBA/LogEntry.php
@@ -9,7 +9,6 @@ class LogEntry {
     public $start;
     public $end;
     public $duration;
-    public $trace;
 
     public function __construct($data, $statement) {
         $this->data = $data;
@@ -25,26 +24,21 @@ class LogEntry {
 
     public function __toString() {
         $s = round($this->duration, 3) . 's ';
+        $s .= ' <b>' . $this->getPrettyStatement() . '</b><br />';
+        return $s;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getPrettyStatement()
+    {
         // Merge parameters into SQL statement
         $statement = $this->statement;
         foreach ($this->data as $key => $value) {
             $statement = str_replace($key, "'" . $value . "'", $statement);
         }
-
-        $s .= ' <b>' . $statement . '</b><br />';
-
-        foreach (array_reverse($this->trace) as $key => $trace) {
-            if ($key > 0) {
-                $s .= '-> ';
-            } else {
-                $s .= '&nbsp;&nbsp; ';
-            }
-            $s .= str_replace(' ', '&nbsp;', str_pad($trace['line'], 4, ' ', STR_PAD_LEFT)) . ' ' . $trace['class'] . $trace['type'] . $trace['function'] . "()<br />";
-        }
-
-
-        return $s;
+        return $statement;
     }
 
 }
